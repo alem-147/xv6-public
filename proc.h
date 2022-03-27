827866f2476fef1972a6b3bda21975afd3f6b92b
Alex Maliwat <atsm360@gmail.com>
2022-03-27 19:42:45 +0000

    add fields for time ticketing

--- proc.h
+++ proc.h
@@ -51,6 +51,8 @@ struct proc {
   char name[16];               // Process name (debugging)
 	int exit_status;						 // exit status for proc
 	int priority_val;						 // priority value for process range: 0 (h) -> 31 (l)
+	int T_start;								 // tick start time
+	int T_finish;								 // tick finish time
 };
 
 // Process memory is laid out contiguously, low addresses first:
