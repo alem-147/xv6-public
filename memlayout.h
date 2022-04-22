849ff76beaa42310be17a36fe5cd06e447ea96ea
Alex Maliwat <atsm360@gmail.com>
2022-04-22 02:00:41 +0000

    defines stackframe macro

--- memlayout.h
+++ memlayout.h
@@ -7,6 +7,7 @@
 // Key addresses for address space layout (see kmap in vm.c for layout)
 #define KERNBASE 0x80000000         // First kernel virtual address
 #define KERNLINK (KERNBASE+EXTMEM)  // Address where kernel is linked
+#define STACKFRAME (KERNBASE -0x4)  // base of user stack (top of user space)
 
 #define V2P(a) (((uint) (a)) - KERNBASE)
 #define P2V(a) ((void *)(((char *) (a)) + KERNBASE))
